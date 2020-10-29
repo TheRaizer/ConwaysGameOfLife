2fde2a3277e2433ebc265055f29c9a8eb7d90eae
TheRaizer <aidan.fu000@gmail.com>
2020-10-29 19:49:13 -0400

    comments added

--- ConwaysGameOfLife/Grid.cs
+++ ConwaysGameOfLife/Grid.cs
@@ -71,14 +71,29 @@ namespace ConwaysGameOfLife
             Rectangle rect = new Rectangle(cell.x * CELL_LENGTH, cell.y * CELL_LENGTH, CELL_LENGTH, CELL_LENGTH);
             if (cell.alive)
             {
+                //Black cells are alive
                 gfx.FillRectangle(aliveBrush, rect);
             }
             else
+            {
+                //White cells are dead
                 gfx.FillRectangle(deadBrush, rect);
+            }
         }
 
         private void CheckMooreNeighbours(Cell cell)
         {
+            ///<summary>
+            /// Here we check for the moore neighbours which are the 8 cells surrounding
+            /// the given cell in square shape.
+            /// 
+            /// The cell parameter is always from the current grid.
+            /// 
+            /// We check all neighbours if they are in the grid and they are alive
+            /// we add 1 to the number of live neighbours. This method is used to Check the rules
+            /// and change the state of the given cells corrosponding cell in the output grid
+            /// accordingly.
+            ///</summary>
             int liveNeighbours = 0;
             outputGrid[cell.x, cell.y].alive = cell.alive;
             for (int x = -1; x <= 1; x++)
@@ -103,6 +118,17 @@ namespace ConwaysGameOfLife
 
         private void CheckRules(Cell cellToCheck, Cell cellToChange, int liveNeighbours)
         {
+            ///<summary>
+            /// These are the rules that each cell must follow in accordance
+            /// to conways game of life. They can be modified to create different effects.
+            /// The cell to check will always come from the current grid and the cell to change
+            /// is always from the output grid.
+            /// 
+            /// The reason we need to only change the output grids node is so that all the nodes
+            /// being changed are being affected by neighbours from the same generation.
+            /// 
+            /// The int liveNeighbours is used to check for the rules.
+            ///</summary>
             if (cellToCheck.alive)
             {
                 if (liveNeighbours < 2 || liveNeighbours > 3 || cellToCheck.lifeSpan > MAX_LIFESPAN)
@@ -124,6 +150,12 @@ namespace ConwaysGameOfLife
 
         private bool IsInGrid(int xPos, int yPos, out Cell cell)
         {
+            ///<summary>
+            /// The job of this method is to calculate whether a given x and y
+            /// are contained in the current grid. If it is it will return true and 
+            /// send out the cell at x and y in the current grid.
+            ///</summary>
+            
             if (xPos >= 0 && xPos < maxX && yPos >= 0 && yPos < maxY)
             {
                 cell = currentGrid[xPos, yPos];
